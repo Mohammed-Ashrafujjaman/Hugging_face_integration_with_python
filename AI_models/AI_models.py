35b1fd2233fefe94f3aef6c986589e8e6014fa4f
Mohammed Ashrafujjaman <mashraf9881@gmail.com>
2025-09-28 11:09:24 +1000

    initial project structure

---
+++ AI_models/AI_models.py
@@ -0,0 +1,4 @@
+# our models will integrate here
+# a generative AI and image classifier
+
+# utils folder will contain some sample images for image classifier
---
+++ GUI/gui.py
@@ -0,0 +1,1 @@
+# our GUI coding will go here
