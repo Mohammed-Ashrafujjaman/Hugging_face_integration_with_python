e128aaba3da5ea9f180cdb6fa40810a78566189a
Mohammed Ashrafujjaman <mashraf9881@gmail.com>
2025-09-29 09:10:11 +1000

    GUI implementation Done

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-# this is our main file
-# for now this is only for folder structure
+'''
+Group Name: Sydney Group 17
+Group Members: 4
+Mohammed Ashrafujjaman Hera - 391197
+Pujan Dey  - 395076
+Shaown Imtiaz - 396121
+Al-Amin Dhaly - 395230
+'''
+
+from GUI.gui import guiApp
+
+if __name__ == "__main__":
+    app = guiApp()
+    app.build_gui()
+    app.run()
+    
+"""
+References:
+https://www.pythontutorial.net/tkinter/
+
+This website has all the information and examples for every elements of TKinter.
+"""
